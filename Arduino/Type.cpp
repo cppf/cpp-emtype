fcea09631e6483bd14cb86b0123789f62ded0398
Subhajit Sahu <wolfram77@gmail.com>
2013-06-16 18:09:59 +0800

    TYPE_LENGTH_BYTES constant added
    This is to control the number of bytes used to represent the length of
    an array being used with this library

--- Arduino/Type.cpp
+++ Arduino/Type.cpp
@@ -5,6 +5,20 @@
  * to another
  */
 
+// When arrays are used with this library, their length
+// is indicated with a length field before any array
+// data. The size of this length field in bytes can be
+// controlled by defining the TYPE_LENGTH_BYTES constant
+// before including this library (or by changing it in
+// the header file itself).
+// The default value of TYPE_LENGTH_BYTES is 1 (1 byte)
+// which should be an appropriate value when the application
+// is related to working with low-cost embedded devices,
+// with limited SRAM capacity
+#ifndef TYPE_LENGTH_BYTES
+#define	TYPE_LENGTH_BYTES		1
+#endif
+
 // define shorthand datatypes
 typedef signed char sbyte;
 typedef unsigned int uint;
