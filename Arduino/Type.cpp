4a89abb8a81eabcc4b79bc2fc84f24518c08f7bf
Subhajit Sahu <wolfram77@gmail.com>
2013-06-16 18:29:07 +0800

    short hand datatypes added

--- Arduino/Type.cpp
+++ Arduino/Type.cpp
@@ -11,19 +11,53 @@
 // controlled by defining the TYPE_LENGTH_BYTES constant
 // before including this library (or by changing it in
 // the header file itself).
+// 
 // The default value of TYPE_LENGTH_BYTES is 1 (1 byte)
 // which should be an appropriate value when the application
 // is related to working with low-cost embedded devices,
 // with limited SRAM capacity
+// 
 #ifndef TYPE_LENGTH_BYTES
 #define	TYPE_LENGTH_BYTES		1
 #endif
 
-// define shorthand datatypes
-typedef signed char sbyte;
+
+// When "getting" or "putting" values without specifying
+// a source or destination, causes this library to make
+// use of its internal buffer for fetching or storing data.
+//
+// "putting" more data than this internal buffer size may
+// cause the operation to fail, or even may cause running
+// application to crash. When "getting", in addition to the
+// above problems, it could also be possible to get garbage
+// data.
+//
+// To avoid all these circumstances, define the value of
+// TYPE_BUFFER_SIZE to the maximum utilized size in the
+// application (before including this library)
+// 
+#ifndef	TYPE_BUFFER_SIZE
+#define	TYPE_BUFFER_SIZE		32
+#endif
+
+// shorthand datatypes
+typedef signed char	sbyte;
+typedef int int16;
 typedef unsigned int uint;
+typedef unsigned int uint16;
+typedef long long32;
 typedef unsigned long ulong;
+typedef unsigned long ulong32;
 typedef char* string;
 #ifndef null
 #define null ((void*)0)
 #endif
+typedef struct _long64
+{
+	long Long[2];
+}long64;
+typedef struct _ulong64
+{
+	ulong Ulong[2];
+}ulong64;
+
