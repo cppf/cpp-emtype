30c1949feff3c3a09ce73d849fb892a05e90af7b
Subhajit Sahu <wolfram77@gmail.com>
2013-06-16 17:58:50 +0800

    added a few shorthand datatypes

---
+++ Arduino/Type.cpp
@@ -0,0 +1,15 @@
+/*
+ * Type Library
+ *
+ * This library converts data from one type
+ * to another
+ */
+
+// define shorthand datatypes
+typedef signed char sbyte;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef char* string;
+#ifndef null
+#define null ((void*)0)
+#endif
